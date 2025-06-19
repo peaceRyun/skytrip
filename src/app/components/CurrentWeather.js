bf363a088302916a0a153231a9fb2b7c417db37c
seming2 <popqr3@naver.com>
2025-06-19 22:49:13 +0900

    home 화면 컴포넌트 구현/swiper 설치/

---
+++ src/app/components/CurrentWeather.js
@@ -0,0 +1,10 @@
+export default function CurrentWeather() {
+  return (
+    <section className='text-center space-y-1 '>
+      <p className=' text-sm text-blue-100'>나의 위치</p>
+      <h1 className='text-3xl text-blue-100'>서울 강남구</h1>
+      <p className='text-lg'>맑음</p>
+      <p className='text-xl font-semibold'>27°C</p>
+    </section>
+  );
+}
